ebac383405356a16eb32b5666f5110f3903ef30d
Vicc <victoire21@wp.pl>
2019-04-08 17:35:01 +0200

    get_help has been added

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,13 @@
 print('To jest prosty kalkulator.)
+
+def get_help():
+	print("Podaj dwie liczby: ")
+
 def dodaj():
 	a = int(input())
 	b = int(input())
 	print(a+b)
 
 	
+get_help()
 print("Koniec programu")
